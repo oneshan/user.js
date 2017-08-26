32e4877b01e65d44baaac59613e90a0e5c0abc80
oneshan <oneshan@users.noreply.github.com>
2017-08-26 13:09:53 -0700

    ptt_script: 增加顯示推文樓數

--- ptt_script.js
+++ ptt_script.js
@@ -1,12 +1,12 @@
-// ==UserScript==
+// ==Use// ==UserScript==
 // @name         PTT Userscript
 // @namespace    http://github.com/oneshan
-// @version      0.1
-// @description  自動跳過年齡分級, 推文顯示黑人問號
+// @version      0.2
+// @description  自動跳過年齡分級, 推文顯示黑人問號,樓數
 // @author       oneshan
 // @include      https://www.ptt.cc/*
 // @grant        none
-// ==/UserScript==a
+// ==/UserScript==
 
 (function(location) {
 
@@ -15,19 +15,25 @@
         $('button:contains("我同意，我已年滿十八歲")').click();
     }
 
-    // 推文顯示黑人問號
-    function image() {
+    
+    function modify_push() {
         var foo = document.getElementsByClassName('push-content');
         for (var idx = 0; idx < foo.length; ++idx) {
+            
+            // 推文顯示黑人問號
             var domImgNode = document.createElement("img");
             domImgNode.alt = "黑人問號";
             domImgNode.src = "https://emos.plurk.com/2b2772dc330f168a7c72f29ca6460cc9_w48_h48.jpeg";
             if (foo[idx].textContent.indexOf("黑人問號") > 0) {
                 foo[idx].appendChild(domImgNode);
             }
+
+            // 顯示推文樓數
+            domTextNode = document.createTextNode(idx + 1 + "F ");
+            foo[idx].parentNode.prepend(domTextNode);
         }
     }
 
-    image();
+    modify_push();
 
 })();
