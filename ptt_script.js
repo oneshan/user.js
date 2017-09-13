df40c31bf4f8c4f9f4aae49305e201b8323e4141
oneshan <oneshan@users.noreply.github.com>
2017-09-13 09:23:23 -0700

    ptt_script: Highlight author's id

--- ptt_script.js
+++ ptt_script.js
@@ -1,8 +1,8 @@
 // ==Use// ==UserScript==
 // @name         PTT Userscript
 // @namespace    http://github.com/oneshan
-// @version      0.2
-// @description  自動跳過年齡分級, 推文顯示黑人問號,樓數
+// @version      0.3
+// @description  自動跳過年齡分級, 推文顯示黑人問號,樓數,高亮作者ID
 // @author       oneshan
 // @include      https://www.ptt.cc/*
 // @grant        none
@@ -15,11 +15,22 @@
         $('button:contains("我同意，我已年滿十八歲")').click();
     }
 
-    
+    // Highlight 推文作者ID
+    function hl_userid() {
+        var author = document.getElementsByClassName('article-meta-value')[0].textContent;
+        author = author.split(' ')[0];
+        var foo = document.getElementsByClassName('push-userid');
+        for (var idx = 0; idx < foo.length; ++idx) {
+            if (foo[idx].textContent == author) {
+                foo[idx].style.backgroundColor = "#20B2AA";
+            }
+        }
+    }
+
     function modify_push() {
         var foo = document.getElementsByClassName('push-content');
         for (var idx = 0; idx < foo.length; ++idx) {
-            
+
             // 推文顯示黑人問號
             var domImgNode = document.createElement("img");
             domImgNode.alt = "黑人問號";
@@ -35,5 +46,5 @@
     }
 
     modify_push();
-
+    hl_userid();
 })();
